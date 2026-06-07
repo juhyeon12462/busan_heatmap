ae390db1dada5cf26325378d78e7e39828c12722
juhyeon12462 <wngus1852@naver.com>
2026-06-07 23:05:42 +0900

    Update vite.config.ts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "https://busan-heatmap.onrender.com",
         changeOrigin: true,
       },
     },
